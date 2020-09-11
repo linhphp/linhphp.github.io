7cc950c54da6e5ca07310216cc82aac5695730f0
linhphp <thuclinh997@gmail.com>
2020-09-11 09:57:59 +0700

    up phan san pham

--- 1.js
+++ 1.js
@@ -1,4 +1,24 @@
-﻿ $(function(){
- 
-})  
+﻿$(function(){
+
+	vitriedu = $('#khoi_edu').offset().top;
+	// console.log(vitriedu);
+	$('.nutxuong,.kn').click(function(){
+		$('body,html').animate({scrollTop:vitriedu},600);
+		// console.log('click duoc chua');
+		return false;
+	});
+	sp = $('#sp').offset().top;
+	$('.sp').click(function(){
+		$('body,html').animate({scrollTop:sp},600);
+		// console.log('click duoc chua');
+		return false;
+	});
+	$('.about').click(function(){
+		$('body,html').animate({scrollTop:0},800);
+		// console.log('click duoc chua');
+		return false;
+	});
+	
+	
+});
  
