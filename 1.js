deee97e36772326868334348e58515fc36b486be
linhphp <thuclinh997@gmail.com>
2020-09-11 13:00:00 +0700

    scroll

--- 1.js
+++ 1.js
@@ -4,18 +4,14 @@
 	// console.log(vitriedu);
 	$('.nutxuong,.kn').click(function(){
 		$('body,html').animate({scrollTop:vitriedu},600);
-		// console.log('click duoc chua');
 		return false;
 	});
-	sp = $('#sp').offset().top;
 	$('.sp').click(function(){
-		$('body,html').animate({scrollTop:sp},600);
-		// console.log('click duoc chua');
+		$('body,html').animate({scrollTop:$('#sp').offset().top},600);
 		return false;
 	});
 	$('.about').click(function(){
 		$('body,html').animate({scrollTop:0},800);
-		// console.log('click duoc chua');
 		return false;
 	});
 	$('.cmt').click(function(){
