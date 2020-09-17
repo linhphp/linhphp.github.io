856ad26135ba1450e590a568f4e7c6cc89cc0df1
linhphp <thuclinh997@gmail.com>
2020-09-17 20:25:18 +0700

    push lai

---
+++ 1.js
@@ -0,0 +1,27 @@
+﻿$(function(){
+
+	vitriedu = $('#khoi_edu').offset().top;
+	// console.log(vitriedu);
+	$('.nutxuong,.kn').click(function(){
+		$('body,html').animate({scrollTop:vitriedu},600);
+		return false;
+	});
+	$('.sp').click(function(){
+		$('body,html').animate({scrollTop:$('#sp').offset().top},600);
+		return false;
+	});
+	$('.about').click(function(){
+		$('body,html').animate({scrollTop:0},800);
+		return false;
+	});
+	$('.cmt').click(function(){
+		$('body,html').animate({scrollTop:$('#comment').offset().top},600);
+		return false;
+	});
+	$('.lh').click(function(){
+		$('body,html').animate({scrollTop:$('#lienhe').offset().top},600);
+		return false;
+	});
+	
+});
+ 
